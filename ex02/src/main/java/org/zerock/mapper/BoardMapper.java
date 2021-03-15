43edbb538216bf162d9acb0bce4e2f97ff6fcb68
SangMin-Code <ragdexplorer@gmail.com>
2021-03-15 20:07:21 +0900

    2021.03.15 _CRUD, Mapper

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,4 +7,15 @@ import org.zerock.domain.BoardVO;
 public interface BoardMapper {
 	
 	public List<BoardVO>getList();
+	
+	public void insert(BoardVO board); //PK가 필요없는경우
+	
+	public void insertSelectKey(BoardVO board); //PK가 필요없는경우
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
 }
