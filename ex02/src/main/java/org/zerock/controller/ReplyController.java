83930bea82570bf62f06749b55c1123b955c4ab1
SangMin-Code <ragdexplorer@gmail.com>
2021-03-25 18:36:40 +0900

    2021.03.25 _ test

--- ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,6 +1,18 @@
 package org.zerock.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.service.ReplyService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/replies/")
+@RestController
+@Slf4j
+@AllArgsConstructor
 public class ReplyController {
 	
+	private ReplyService service;
 	
 }
