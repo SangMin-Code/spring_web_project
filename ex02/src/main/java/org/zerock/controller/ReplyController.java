65e3f72905daea22cb5c7bb5946d935cc3e7125f
SangMin-Code <ragdexplorer@gmail.com>
2021-03-25 18:54:11 +0900

    2021.03.25 등록

--- ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,7 +1,13 @@
 package org.zerock.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +20,24 @@ import lombok.extern.slf4j.Slf4j;
 public class ReplyController {
 	
 	private ReplyService service;
-	//test
+	
+	
+	@PostMapping(value="/new",
+			consumes = "application/json",
+			produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo){
+		
+		log.info("ReplyVO: "+vo);
+		
+		int insertCount = service.register(vo);
+		
+		log.info("Reply INSERT COUNT : "+insertCount);
+		
+		return insertCount ==1 ?
+				new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		
+	}
 	
 }
