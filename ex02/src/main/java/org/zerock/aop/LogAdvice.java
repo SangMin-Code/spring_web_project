29fe282af99b85b351005ef9396f1f98e0bba6ad
SangMin-Code <ragdexplorer@gmail.com>
2021-03-30 15:59:12 +0900

    2021.03.30 _ AOP

---
+++ ex02/src/main/java/org/zerock/aop/LogAdvice.java
@@ -0,0 +1,53 @@
+package org.zerock.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAdvice {
+
+	@Before("execution(* org.zerock.service.AOPSampleService*.*(..))")
+	public void logBefore() {
+		log.info("============");
+	}
+	@Before("execution(* org.zerock.service.AOPSampleService*.doAdd(String,String)) && args(str1,str2)")
+	public void logBeforeWithParam(String str1, String str2) {
+		log.info("str1: "+str1);
+		log.info("str2: "+str2);
+	}
+	@AfterThrowing(pointcut = "execution(* org.zerock.service.AOPSampleService*.*(..))", throwing="exception")
+	public void logException(Exception exception) {
+		log.info("Exception....!!!!!");
+		log.info("exception: "+exception);
+	}
+	@Around("execution(* org.zerock.service.AOPSampleService*.*(..))")
+	public Object logTime(ProceedingJoinPoint pjp) {
+		
+		long start = System.currentTimeMillis();
+		
+		log.info("Target: "+pjp.getTarget());
+		log.info("Param: "+Arrays.toString(pjp.getArgs()));
+		//invoke method
+		Object result = null;
+		try {
+			result = pjp.proceed();
+		}catch(Throwable e) {
+			//TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		log.info("Time :"+(end-start));
+		return result;
+	}
+}
