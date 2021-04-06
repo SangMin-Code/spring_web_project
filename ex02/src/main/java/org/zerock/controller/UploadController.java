ec0370952c50872596c2f50abdbf27bbfd1c5a3f
SangMin-Code <ragdexplorer@gmail.com>
2021-04-06 20:14:27 +0900

    2021.04.06_파일 업로드

---
+++ ex02/src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,74 @@
+package org.zerock.controller;
+
+import java.io.File;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class UploadController {
+	
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		
+		String uploadFolder = "C:\\upload";
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("------------------");
+			log.info("Upload File Name :"+multipartFile.getOriginalFilename());
+			log.info("Upload File Size :"+multipartFile.getSize());
+			
+			File saveFile = new File(uploadFolder,multipartFile.getOriginalFilename()); 
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}				
+		}
+	}
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		log.info("upload ajax");
+	}
+	
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("update ajax post......");
+		
+		String uploadFolder = "C:\\upload";
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			log.info("-------------");
+			log.info("Upload File Name :"+multipartFile.getOriginalFilename());
+			log.info("Upload File Size :"+multipartFile.getSize());
+			
+			String uploadfileName = multipartFile.getOriginalFilename();
+			
+			uploadfileName = uploadfileName.substring(uploadfileName.lastIndexOf("\\")+1);
+			log.info("only file name :"+uploadfileName);
+			
+			File saveFile = new File(uploadFolder,uploadfileName);
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+	}
+
+}
